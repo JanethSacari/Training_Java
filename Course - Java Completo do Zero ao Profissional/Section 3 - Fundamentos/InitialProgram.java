a8f1621e8dce6e8d2a731b4ce495e726ad68df02
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-08-06 23:39:15 -0300

    Training Java - Adding new popular print.

--- Course - Java Completo do Zero ao Profissional/Section 3 - Fundamentos/InitialProgram.java
+++ Course - Java Completo do Zero ao Profissional/Section 3 - Fundamentos/InitialProgram.java
@@ -3,5 +3,6 @@ package fundamentos;
 public class InitialProgram {
   public static void main(String ... args) {
     System.out.println("First program!");
+    System.out.println("Hello World");
   }
 }
