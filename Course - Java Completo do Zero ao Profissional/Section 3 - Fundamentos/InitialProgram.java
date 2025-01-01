904d9ed3603aebfc4121f812e8f031dda89616ca
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-12-31 21:36:01 -0300

    Training Java - Training Variables

--- Course - Java Completo do Zero ao Profissional/Section 3 - Fundamentos/InitialProgram.java
+++
@@ -1,8 +0,0 @@
-package fundamentos;
-
-public class InitialProgram {
-  public static void main(String ... args) {
-    System.out.println("First program!");
-    System.out.println("Hello World");
-  }
-}
